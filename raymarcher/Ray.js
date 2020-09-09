6d1d39fb6e335f6aa2676611825c351f3a06fb27
Dayshadow <42987997+Dayshadow@users.noreply.github.com>
2020-09-08 21:29:25 -0400

    Functional 2d (not box tho for some reason)

--- raymarcher/Ray.js
+++ raymarcher/Ray.js
@@ -2,15 +2,17 @@ class Ray {
     constructor(v, loc) { // v is a normalized vector and loc is the starting location for the ray
         this.v = v.normalize();
         this.loc = loc;
-        this.stepSize = 2;
-        this.minStep = 1; // Anytime the step size gets below 1 it registers as a collision
+        this.stepSize = 5;
+        this.minStep = 3; // Anytime the step size gets below 1 it registers as a collision
         this.maxStep = 1000; // If the step size is ever over 1000 it registers as a miss
-        this.debug = 0;
 
     }
     testIntersection() {
         let lowestDist = Infinity;
+        let count = 0
         while (this.stepSize > this.minStep) {
+            if (this.stepSize > this.maxStep) return;
+
             for (let obj of world.objs) {
                 let dist = obj.dist(this.v.addVector(this.loc))
                 if (dist < lowestDist) {
@@ -18,13 +20,12 @@ class Ray {
                 }
             }
             this.stepSize = lowestDist;
+            lowestDist = Infinity;
+            ctx.beginPath();
+            ctx.arc(this.v.x + this.loc.x, this.v.y + this.loc.y, Math.abs(this.stepSize), 0, Math.PI * 2);
+            ctx.stroke();
             this.v = this.v.march(this.stepSize);
-            this.debug++;
-            console.log(this.v)
-            if(this.debug > 20) {
-                break;
-            }
         }
-        ctx.fillRect(this.v.addVector(this.loc).x, this.v.addVector(this.loc).y, 10, 10)
+        return this.v.addVector(this.loc);
     }
 }
--- raymarcher/raymarcher.js
+++ raymarcher/raymarcher.js
@@ -6,18 +6,25 @@ c.height = ch = window.innerHeight;
 
 let renderer = new Renderer2d()
 
-let box1 = new Box(new Vector3(20, 20, 0), new Vector3(400, 500, 0));
-let sphere1 = new Sphere(20, new Vector3(500, 200, 0))
+//let box1 = new Box(new Vector3(20, 20, 0), new Vector3(400, 500, 0));
+let sphere1 = new Sphere(Math.random() * 100 + 30, new Vector3(Math.random() * cw/2 + cw/4, Math.random() * ch/2 + ch/4, 0))
+let sphere2 = new Sphere(Math.random() * 100 + 30, new Vector3(Math.random() * cw/2 + cw/4, Math.random() * ch/2 + ch/4, 0))
+let sphere3 = new Sphere(Math.random() * 100 + 30, new Vector3(Math.random() * cw/2 + cw/4, Math.random() * ch/2 + ch/4, 0))
+let sphere4 = new Sphere(Math.random() * 100 + 30, new Vector3(Math.random() * cw/2 + cw/4, Math.random() * ch/2 + ch/4, 0))
 
 let world = new World();
 
-world.addObject(box1);
+//world.addObject(box1);
 world.addObject(sphere1);
-
+world.addObject(sphere2);
+world.addObject(sphere3);
+world.addObject(sphere4);
+let f = 0
 function mainLoop() {
+    f += Math.PI / 360;
     ctx.clearRect(0, 0, cw, ch);
     renderer.newFrame();
-    let ray = new Ray(new Vector3(-1,0,0), new Vector3(mouse.x, mouse.y, 0))
+    let ray = new Ray(new Vector3(Math.cos(f),Math.sin(f),0), new Vector3(mouse.x, mouse.y, 0))
     ray.testIntersection();
     requestAnimationFrame(mainLoop);
 }
