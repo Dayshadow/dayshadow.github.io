4752110148727593c1458ba2cee154175f60e823
Dayshadow <42987997+Dayshadow@users.noreply.github.com>
2020-09-09 14:43:06 -0400

    ray density setting

--- raymarcher/Renderer.js
+++ raymarcher/Renderer.js
@@ -4,15 +4,16 @@ class Renderer {
     }
     newFrame() {
         image.getImage(); // have to initialize the internal imagedata variable so it knows what to do with it
-        for (let y = 0; y < ch; y++) { // looping through all x's and y's of the canvas
-            for (let x = 0; x < cw; x++) {
+        for (let y = 0; y < ch; y += 1 / rayDensity) { // looping through all x's and y's of the canvas
+            for (let x = 0; x < cw; x += 1 / rayDensity) {
                 let screenCoord = new Vector3((2 * x) / image.imageData.width - 1, (-2 * y) / image.imageData.height + 1); // Code that generates the -1 to +1 coords the camera needs
 
                 let ray = camera.makeRay(screenCoord); // tellin the camera to do it's thing
                 let result = ray.intersection();
 
                 if (result) {
-                    image.setPixel(x, y, 0, 0, 0, 255 * result / 21); // result is the amount of steps it took to march into it's intersection
+
+                    image.setPixel(Math.floor(x + Math.random() * (1 / rayDensity)), Math.floor(y + Math.random() * (1 / rayDensity)), 0, 0, 0, 255 * result / 21); // result is the amount of steps it took to march into it's intersection
                     // makes a nice ambient occlusion
                 }
             }
--- raymarcher/raymarcher.js
+++ raymarcher/raymarcher.js
@@ -1,6 +1,7 @@
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
-let resDiv = 8.0; // Changes the denominator of the res
+let resDiv = 2.0; // Changes the denominator of the res
+let rayDensity = 0.7;
 c.width = cw = window.innerWidth / resDiv;
 c.height = ch = window.innerHeight / resDiv;
 
