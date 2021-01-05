df0573889b34544ebb7937902435de6c4ec3fd12
Dayshadow <42987997+Dayshadow@users.noreply.github.com>
2021-01-05 13:24:49 -0500

    probably wont work

--- DaybotWork/websocket.js
+++ DaybotWork/websocket.js
@@ -1,5 +1,5 @@
 
-    let ws = new WebSocket("ws://24.63.238.100:3000");
+    let ws = new WebSocket("wss://24.63.238.100:3000");
     
     ws.onmessage = (message) => {
         msg = JSON.parse(message.data);
