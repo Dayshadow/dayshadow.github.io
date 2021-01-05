501de86323514de0a04623573a90b7eb0e888617
Dayshadow <42987997+Dayshadow@users.noreply.github.com>
2021-01-05 13:37:00 -0500

    ok

--- DaybotWork/websocket.js
+++ DaybotWork/websocket.js
@@ -1,6 +1,9 @@
 
     let ws = new WebSocket("wss://24.63.238.100:3000");
     
+    ws.onopen(() => {
+        ws.send(JSON.stringify({content: "hello"}));
+    })
     ws.onmessage = (message) => {
         msg = JSON.parse(message.data);
         if (msg.type === "") {
@@ -8,4 +11,3 @@
         } 
     }
 
-ws.send(JSON.stringify({content: "hello"}));
