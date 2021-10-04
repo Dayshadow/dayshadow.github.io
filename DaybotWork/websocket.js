dba46b3c2b6c59a328ab26e0af88b3a6f90daa22
Dayshadow <42987997+Dayshadow@users.noreply.github.com>
2021-10-04 17:51:16 -0400

    Deleted DaybotWork
    Not In use, hosted on my own server

--- DaybotWork/websocket.js
+++
@@ -1,9 +0,0 @@
-
-    let ws = new WebSocket("wss://24.63.238.100:3000");
-    
-    ws.onmessage = (message) => {
-        msg = JSON.parse(message.data);
-        if (msg.content === "b") {
-            ws.send(JSON.stringify({content: "hello"}));
-        } 
-    }
